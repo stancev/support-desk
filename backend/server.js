09e835f6f95b69d6f89a8422967107ca6dddefab
Aleksandar Stancev <aleksandar.b.stancev@telenor.rs>
2023-02-18 20:07:48 +0100

    Added register user and login features

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 8000;
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
