3c1991396a95198b75985f72cd1ac09b4927a808
Aleksandar Stancev <aleksandar.b.stancev@telenor.rs>
2023-02-18 23:26:57 +0100

    Added frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,6 +59,15 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -68,4 +77,5 @@ const generateToken = id => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
